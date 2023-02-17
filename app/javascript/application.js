bd8ba4c597b2a38b68652a86d410eb6c8601badc
Marco Oquendo <marcooquendoc@gmail.com>
2023-02-17 18:51:39 -0500

    Manage alerts

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,12 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+const closeButton = document.querySelector('[data-dismiss-target]');
+
+if(closeButton) {
+  closeButton.addEventListener('click', () => {
+    const target = document.querySelector(closeButton.dataset.dismissTarget);
+    target.classList.add('invisible');
+  });
+}
