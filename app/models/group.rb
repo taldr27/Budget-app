cdff566a38571e5738a37b40fdfe88b9b30ccad0
Marco Oquendo <marcooquendoc@gmail.com>
2023-02-17 13:15:20 -0500

    Add validations and relationships to models

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,3 +1,11 @@
 class Group < ApplicationRecord
-  belongs_to :user
+  belongs_to :author, class_name: 'User'
+  has_many :deal_groups, dependent: :destroy
+  has_many :deals, through: :deal_groups, dependent: :destroy
+
+  validates :name, presence: true, length: { maximum: 15 }
+
+  def total_deals
+    deals.sum(:amount)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
+
+  validates :first_name, presence: true, length: { maximum: 25 }
+  validates :last_name, presence: true, length: { maximum: 25 }
+  validates :email, presence: true, length: { maximum: 50 }
+  validates :password, presence: true, length: { minimum: 6 }
+
+  has_many :groups, foreign_key: 'author_id', dependent: :destroy
+  has_many :deals, foreign_key: 'author_id', dependent: :destroy
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
 end
