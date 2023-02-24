bfdfb9ff8487fa05a9dc9df6914a52fa06ff1c9f
Marco Oquendo <marcooquendoc@gmail.com>
2023-02-24 18:06:35 -0500

    Correct offenses and update migrations

--- Gemfile
+++ Gemfile
@@ -19,10 +19,10 @@ gem 'turbo-rails'
 gem 'tzinfo-data', platforms: %i[mingw mswin x64_mingw jruby]
 
 group :development, :test do
+  gem 'database_cleaner'
   gem 'debug', platforms: %i[mri mingw x64_mingw]
   gem 'rails-controller-testing'
   gem 'rspec-rails'
-  gem 'database_cleaner'
 end
 
 group :development do
--- db/migrate/20230216054743_create_expenses.rb
+++ db/migrate/20230216054743_create_expenses.rb
@@ -1,6 +1,6 @@
-class CreateDeals < ActiveRecord::Migration[7.0]
+class CreateExpenses < ActiveRecord::Migration[7.0]
   def change
-    create_table :deals do |t|
+    create_table :expenses do |t|
       t.references :author, null: false, foreign_key: {to_table: :users}
       t.string :name
       t.decimal :amount
